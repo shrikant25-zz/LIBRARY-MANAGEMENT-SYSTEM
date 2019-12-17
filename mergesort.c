b65d2dc251ee62d79bd491872554d18c292f56ab
shrikant <shrikantwankhede24@gmail.com>
2019-12-17 19:00:32 +0530

    comments added in mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,3 +1,6 @@
+//this program accepts a linked list and sorts the lsit with merge sort
+//the merge sort is modified according to the requirements of the program
+//along with the list the sort function accepts a character whhich the first word of the category of book(for more chech declared.h)
 
 
 #include <stdio.h>
@@ -7,15 +10,14 @@
 
 
 
-struct book* sortedmerge(struct book *, struct book*, char);
-void split(struct book *, struct book **, struct book **);
-struct book* mergesort(struct book*,char);
-int compare_strings(char *a,char *b);
-int compare_int(int a,int b);
-int compare_int(int a,int b);
+struct book* sortedmerge(struct book *, struct book*, char);   //the basic function to accept a list
+void split(struct book *, struct book **, struct book **);     //this function splits the list
+struct book* mergesort(struct book*,char);                     //this function merges the two lists
+int compare_strings(char *a,char *b);                          //function to compare two strings
+int compare_int(int a,int b);                                  //function to compare two integers
 
 
-struct book* mergesort(struct book* h,char n)
+struct book* mergesort(struct book* h,char n)                   
 {
 	struct book* head = h;
 	struct book* a = NULL;
@@ -25,7 +27,7 @@ struct book* mergesort(struct book* h,char n)
 	if ((head == NULL) || (head->next == NULL))
        return head;
 
-	split(head,&a,&b);
+	split(head,&a,&b);                                          
 
 	a = mergesort(a,n);
 	b = mergesort(b,n);
@@ -34,7 +36,7 @@ struct book* mergesort(struct book* h,char n)
 	return head;
 }
 
-struct book* sortedmerge(struct book* a, struct book* b, char n)
+struct book* sortedmerge(struct book* a, struct book* b, char n)    //accepts two string and a character
 {
 	struct book* result = NULL;
     int comparision_flag = 0;
@@ -43,7 +45,7 @@ struct book* sortedmerge(struct book* a, struct book* b, char n)
 	else if (b == NULL)
 		return (a);
 
-    switch(n)
+    switch(n)                                                        //checks the character and calls the compare function and sets the comparison_flag accordingly
     {
       case 'C'    :  comparision_flag = compare_strings(a->Category, b->Category);break;
       case 'I'    :  comparision_flag = compare_strings(a->Book_ID, b->Book_ID);break;
@@ -55,49 +57,50 @@ struct book* sortedmerge(struct book* a, struct book* b, char n)
     };
 
 
-	if (comparision_flag)
-    {
-		result = a;
-		result->next = sortedmerge(a->next, b, n);
+	if (comparision_flag)                                       //if comparision_flag is true it means the first string is smaller
+         {                                                          //hence it will be added first in the list
+		result = a;                                            
+		result->next = sortedmerge(a->next, b, n);         //recursivley calls itself untill an exit condition is encountered
 	}
 	else
 	{
-		result = b;
-		result->next = sortedmerge(a, b->next, n);
+		result = b;                                        //if compariosion_flag is false it means the second string is smaller
+		result->next = sortedmerge(a, b->next, n);          //hence it will be added first in the list
 	}
 	return (result);
 }
 
-void split(struct book* source,struct book** front, struct book** back)
-{
-	struct book* fast;
+void split(struct book* source,struct book** front, struct book** back)  //this function splits the given string in two parts, it accepts the pointer to the head of the string and address of other two pointers 
+{                                                                        //the other two pointers are used two store the two parts of the string after splitting.
+	struct book* fast;                                               //[ source->1->2->3->4->NULL ]
 	struct book* slow;
-	slow = source;
-	fast = source->next;
+	slow = source;                                                      //pointer 'slow' is assigned the address of first element of the string 
+	fast = source->next;                                                //pointer  'fast' is assigned the address of the element next to first element(i.e second element)
 
-	while (fast != NULL) {
-		fast = fast->next;
+	while (fast != NULL) 
+	{                                                          //untill the fast pointer reaches NULL the loop continues
+		fast = fast->next;                                 //the 'fast' pointer is incremented twice while the 'slow' pointer is incremented only once per iterarion
 		if (fast != NULL) {
-			slow = slow->next;
+			slow = slow->next;                         //both the pointer are incremented
 			fast = fast->next;
 		}
-	}
-
-	(*front) = source;
-	(*back) = slow->next;
-	slow->next = NULL;
-}
+	}                                                          //because of such incrementation the 'fast' pointer reaches the end of the string and at the same time 
+                                                                   //th slow pointer reaches at the the center of string
+	(*front) = source;                                         //'front' pointer is assigned the address of head os the string
+	(*back) = slow->next;                                      //while the 'back' pointer is assigned the address of the element next to the element at center
+	slow->next = NULL;                                         //the 'slow' is set to store NULL in its address storing ('next') place, resulting in the split of string.
+}                                                                  // [ source->1->2->3->4->NULL ]  ....  after split [ front->1->2->NULL and back->3->4->NULL ]    
 
 
 int compare_int(int a,int b)
 {
-  if(a < b)
+  if(a < b)                                                        //compares the integers and returns the value accordingly
     return 1;
     return 0;
 }
 
 
-int compare_strings(char *a,char *b)
+int compare_strings(char *a,char *b)                                //compares the string and returns the value accordingly
 {
   int c;
   c = strcmp(a,b);
